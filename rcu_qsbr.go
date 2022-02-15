9a36fd42a45dfa91b81876e1beca8f59502ddcea
singchia <singchia@163.com>
2022-02-15 22:08:14 +0800

    划水

--- rcu_qsbr.go
+++ rcu_qsbr.go
@@ -1,5 +1,17 @@
 package gorcu
 
+import "unsafe"
+
 type RCUQsbr struct {
 	token, ackedToken uint64
+	numGoroutines     int
+	cnts              []unsafe.Pointer
+}
+
+type RCUQsbrCnt struct {
+	qsbrCnt uint64
+}
+
+func RcuQsbrInit(rq *RCUQsbr, maxGoroutines int) error {
+	return nil
 }
