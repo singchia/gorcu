0415e6ace5445cedd6dd4c8f2518b2a964b4fb6c
singchia <singchia@163.com>
2022-02-19 21:20:34 +0800

    frame

--- rcu_qsbr.go
+++ rcu_qsbr.go
@@ -1,21 +1,42 @@
 package gorcu
 
-import "unsafe"
+import (
+	"unsafe"
+)
+
+const (
+	qsbrCntInit uint64 = 1
+)
 
 type RCUQsbr struct {
-	token, ackedToken uint64
-	numGoroutines     int
-	cnts              []unsafe.Pointer
+	token         uint64
+	ackedToken    uint64
+	numElems      uint32           // number of elements needed
+	numGoroutines uint32           // number of goroutines currently
+	maxGoroutines uint32           // maximum number of goroutines
+	cnts          []unsafe.Pointer // []*RCUQsbrCnt
+	bitmap        []uint64
 }
 
 type RCUQsbrCnt struct {
 	qsbrCnt uint64
 }
 
-func RcuQsbrInit(rq *RCUQsbr, maxGoroutines int) error {
+func NewRcuQsbr(maxGoroutines uint32) *RCUQsbr {
+	rq := &RCUQsbr{}
+	rq.maxGoroutines = maxGoroutines
+	rq.numElems = ((maxGoroutines + 64 - 1) / 64) * 64
+	rq.token = qsbrCntInit
+	rq.ackedToken = qsbrCntInit - 1
+	rq.cnts = make([]unsafe.Pointer, maxGoroutines)
+	rq.bitmap = make([]uint64, rq.numElems)
+	return rq
+}
+
+func (rq *RCUQsbr) Register(id int) error {
 	return nil
 }
 
-func RcuQsbrRegister(rq *RCUQsbr, id int) error {
+func (rq *RCUQsbr) Unregister(id int) error {
 	return nil
 }
